dc701a80bf49ba15bcd9dd4a0ce248e4327d2d70
pantsel <panagis.tselentis@konghq.com>
2025-04-06 00:35:11 +0200

    wip

--- entities/entity-1/env.dev.sh
+++ entities/entity-1/env.dev.sh
@@ -1,1 +1,4 @@
-export DECK_CONFIG_OIDC_ISSUER="https://auth.example.com/auth/realms/master/.well-known/openid-configuration"
+# Get folder name
+FOLDER_NAME=$(basename $PWD)
+
+export DECK_CONFIG_OIDC_ISSUER="https://$FOLDER_NAME-dev.auth.example.com/auth/realms/master/.well-known/openid-configuration"
