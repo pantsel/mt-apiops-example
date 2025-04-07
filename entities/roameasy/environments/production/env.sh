741447e936fedfd44f59e7e1d60d2899d55c724a
pantsel <panagis.tselentis@konghq.com>
2025-04-07 16:58:18 +0200

    wip

---
+++ entities/roameasy/environments/production/env.sh
@@ -0,0 +1,5 @@
+# Any deck environment specific variables can be set here
+# Variables should be prefixed with DECK_
+
+export DECK_ENTITY_NAME="roameasy-production"
+export DECK_CONFIG_OTEL_SERVICE_NAME="kong-dp-roameasy-production"
