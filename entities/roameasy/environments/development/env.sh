3c6376377dac14c96e53cde977c70fcc8c63986e
pantsel <panagis.tselentis@konghq.com>
2025-04-08 21:24:14 +0200

    wip

--- entities/roameasy/environments/development/env.sh
+++
@@ -1,5 +0,0 @@
-# Any deck environment specific variables can be set here
-# Variables should be prefixed with DECK_
-
-export DECK_ENTITY_NAME="roameasy-development"
-export DECK_CONFIG_OTEL_SERVICE_NAME="kong-dp-roameasy-development"
--- entities/tripwhiz/environments/development/env.sh
+++
@@ -1,5 +0,0 @@
-# Any deck environment specific variables can be set here
-# Variables should be prefixed with DECK_
-
-export DECK_ENTITY_NAME="tripwhiz-development"
-export DECK_CONFIG_OTEL_SERVICE_NAME="kong-dp-tripwhiz-development"
